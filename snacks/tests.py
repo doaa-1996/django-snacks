fbb7fd96c1efa66cc0f7af64b8a2a6b49d43db65
doaa-1996 <doaaobeidat1996@gmail.com>
2021-07-11 16:09:35 +0300

    finish snacks app

--- snacks/tests.py
+++ snacks/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+
+from django.test import TestCase, SimpleTestCase
+from django.urls import reverse
 
 # Create your tests here.
+
+class snacksTests(SimpleTestCase):
+    def test_home_page_status_code(self):
+        url = reverse('home')
+        actual_status_code = self.client.get(url).status_code
+        expected_status_code = 200
+        self.assertEqual(actual_status_code, expected_status_code)
+    def test_about_page_status_code(self):
+        url = reverse('about')
+        actual_status_code = self.client.get(url).status_code
+        expected_status_code = 200
+        self.assertEqual(actual_status_code, expected_status_code)        
